452d7821f2cd5d60a3c74f0d5ae9b58a23fa521a
Benjamin Pachevie <b.pachevie@gmail.com>
2023-08-16 15:45:45 +0200

    2nd exo

---
+++ app/controllers/reviews_controller.rb
@@ -0,0 +1,28 @@
+class ReviewsController < ApplicationController
+  # def new
+  #   @restaurant = Restaurant.find(params[:restaurant_id])
+  #   @review = Review.new
+  # end
+
+  def show
+    @review = Review.find(review_params)
+  end
+
+  def create
+    @restaurant = Restaurant.find(params[:restaurant_id])
+    @review = Review.new(review_params)
+    @review.restaurant = @restaurant
+
+    if @review.save
+      redirect_to restaurant_path(@restaurant)
+    else
+      render 'restaurants/show', status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def review_params
+    params.require(:review).permit(:rating, :content)
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+puts 'Cleaning database...'
+Restaurant.destroy_all
+
+puts 'Creating restaurants...'
+surpriz = { name: 'Surpriz', address: '7 Boundary St, London', phone_number: '0145811798', category: 'french' }
+pny = { name: 'PNY', address: '56A Shoreditch High St, London', phone_number: '0145823798', category: 'chinese' }
+mama_roma = { name: 'Mama Roma', address: '18 Italy St, London', phone_number: '0119823798', category: 'italian' }
+kinoko = { name: 'Kinoko', address: '23 Osaka St, London', phone_number: '0145822298', category: 'japanese' }
+baraque_frite = { name: 'La Baraque a frite', address: '97 Belgian St, London ', phone_number: '0147423798',
+                  category: 'belgian' }
+
+[surpriz, pny, mama_roma, kinoko, baraque_frite].each do |attributes|
+  restaurant = Restaurant.create!(attributes)
+  puts "Created #{restaurant.name}"
+end
+puts 'Finished!'
